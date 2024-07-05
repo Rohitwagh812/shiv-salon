63de8ff9a9d29799ebe01f61c979132461ec52e3
Rohitwagh812 <rohitwagh480@gmail.com>
2024-07-05 18:43:12 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,38 +4,15 @@ import './App.css'
 
 function App() {
   return (
-    <div className='app-page'>
-      {/* <Navbar className='navbar-main'>
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar> */}
-      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
+    <div className='app-page'> 
+      <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary navbar-main">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">Shiv Hair Salon</Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-            <NavDropdown title="Dropdown" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
+            <Nav.Link href="#features">Home</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>  
             <Nav.Link href="#deets">More deets</Nav.Link>
             <Nav.Link eventKey={2} href="#memes">
               Dank memes
